21315614ed3aa5bdaefe735953cceabe2c378621
Riteshn <riteshnishadofficial66@gmail.com>
2026-08-30 17:11:41 +0530

    the bug is fixed now trying to make it a little better

--- src/components/ui/Section.jsx
+++ src/components/ui/Section.jsx
@@ -1,6 +1,6 @@
 export default function Section({ id = "", className = "", children }) {
   return (
-    <section id={id} className={`${className} w-full py-24 relative`}>
+    <section id={id} className={`${className} w-full lg:py-24 py-25 relative`}>
       {children}
     </section>
   );
