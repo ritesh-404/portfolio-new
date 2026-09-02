4b58b0e1a8a0a52f0e079c5afb53996597dbaf19
Riteshn <riteshnishadofficial66@gmail.com>
2026-09-02 16:50:16 +0530

    remove broken CustomCursor import and component usage

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import { Switch, Route } from "wouter";
 import HomePage from "./pages/HomePage";
 import CaseStudyPage from "./pages/CaseStudyPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import CustomCursor from "./components/ui/CustomCursor";
-
 function App() {
   return (
     <>
-      <CustomCursor />
       <Switch>
         <Route path="/" component={HomePage} />
         <Route path="/home" component={HomePage} />
